73a36c10c1dd4c056f2f7dd262f968f0273fe29f
Szrajnert Dominik <d.szrajnert@tme.pl>
2026-05-18 10:52:59 +0200

    empty

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,16 +1,6 @@
 from .models import Task
 from django import forms
 
-
-# class TaskStatusForm(forms.ModelForm):
-#     class Meta:
-#         model = Task
-#         fields = ['done', 'project']
-#         widgets = {
-#             'done': forms.CheckboxInput(attrs={'class': 'task-done-checkbox'})
-#         }
-
-    
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
